6e1838aef81d05f11abef1b4bdba04e70ab89b3e
GSmes <garrett.smestad@gmail.com>
2023-02-01 18:45:13 -0700

    Add donation form & matching logic

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -5,5 +5,42 @@ class DonationsController < ApplicationController
   end
 
   def create
+    campaign = Campaign.find(params[:campaign_id])
+
+    campaign.donations.new(donation_params)
+    campaign.matches.new(match_params) if match_params
+
+    if campaign.save
+      redirect_to campaign_path(campaign)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:donation)
+          .permit(:amount, match: [:kind, :amount, :remaining_balance])
+  end
+
+  def donation_params
+    create_params.except(:match).merge(donor_id: donor_id)
+  end
+
+  def match_params
+    match_params = create_params.delete(:match)
+
+    return nil unless match_params.has_key?(:kind)
+
+    match_params.merge(donor_id: donor_id)
+  end
+
+  def donor_id
+    # NOTE: Would normally use current authed user, but since I've skipped auth/login,
+    #       I'm simply setting the donor to the first donor who hasn't made a donation yet.
+    donor = Donor.left_joins(:donations).where(donations: {id: nil}).first
+
+    donor.present? ? donor.id : FactoryBot.create(:donor).id
   end
 end
--- app/models/donation.rb
+++ app/models/donation.rb
@@ -3,4 +3,57 @@ class Donation < ApplicationRecord
   belongs_to :donor
 
   validates :amount, numericality: { greater_than: 0.00 }
+
+  after_commit :trigger_matching_donations
+
+  private
+
+  def trigger_matching_donations
+    return if match
+    # NOTE: Pull below logic into background job that kicks off matching donations async
+    #       and pull actual matching logic into a service.
+    matches = campaign.matches.active.where("donor_id != '#{donor_id}'")
+    return unless matches.any?
+
+    matches.each do |match|
+      if match.dollar_match?
+        create_dollar_match(match)
+      else
+        create_donor_match(match)
+      end
+    end
+  end
+
+  def create_dollar_match(match)
+    total_potential_match = match.amount * amount
+    match_donation_amount = total_potential_match > match.remaining_balance ?
+                              match.remaining_balance :
+                              total_potential_match
+
+    campaign.donations.create(
+      amount: match_donation_amount,
+      donor: match.donor,
+      match: true
+    )
+
+    new_remaining_balance = match.remaining_balance - match_donation_amount
+    match.update(remaining_balance: new_remaining_balance)
+  end
+
+  def create_donor_match(match)
+    return unless new_campaign_donor?
+
+    campaign.donations.create(
+      amount: match.amount,
+      donor: match.donor,
+      match: true
+    )
+
+    new_remaining_balance = match.remaining_balance - 1
+    match.update(remaining_balance: new_remaining_balance)
+  end
+
+  def new_campaign_donor?
+    campaign.donations.where(donor_id: donor.id).count <= 1
+  end
 end
